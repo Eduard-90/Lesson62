6f0c7bb273a2163b81c60f47e4b8465ea01a14b0
Eduard-90 <eddard1990@gmail.com>
2023-05-05 22:32:29 +0300

    lesson 62

---
+++ model/customer.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const customerSchema = new Schema({
+  name: String,
+  product_id: Number,
+});
+
+const url = "mongodb://127.0.0.1:27017/shop";
+
+const connection = mongoose.createConnection(url, { maxPoolSize: 10 });
+const Customer = connection.model("customer", customerSchema);
+
+export { Customer };
